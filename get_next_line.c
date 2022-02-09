c77f7b256cbf281a157763768375f2e0a846bf86
Eloi Alonso quinonero <ealonso-@cbr8s1.42barcelona.com>
2022-02-09 19:03:47 +0100

    progress2

--- get_next_line.c
+++ get_next_line.c
@@ -6,19 +6,24 @@
 /*   By: ealonso- <ealonso-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/08 15:40:52 by ealonso-          #+#    #+#             */
-/*   Updated: 2022/02/09 17:03:17 by ealonso-         ###   ########.fr       */
+/*   Updated: 2022/02/09 19:03:11 by ealonso-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#include <string.h>
 
 char	*get_next_line(int fd)
 {
-	static char	buf[33];
+	static char	buf[BUFFER_SIZE];
+	static char	rest[BUFFER_SIZE];
+	char		str[BUFFER_SIZE];
 
 	if (!fd)
 		return (NULL);
-	read (fd, buf, 32);
+	read (fd, buf, BUFFER_SIZE);
+	if (ft_strchr(buf, '\n'))
+	
 	return (buf);
 }
 
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: ealonso- <ealonso-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/08 17:10:06 by ealonso-          #+#    #+#             */
-/*   Updated: 2022/02/08 19:25:26 by ealonso-         ###   ########.fr       */
+/*   Updated: 2022/02/09 18:46:46 by ealonso-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,79 @@
 # include <stdio.h>
 # include <fcntl.h>
 
+char	*ft_strchr(const char *s, int c)
+{
+	int		i;
+	char	*r;
+
+	i = 0;
+	while (s[i] != (char)c && s[i] != '\0')
+		i++;
+	r = (char *)s + i;
+	if (s[i] != (char)c)
+		return (0);
+	else
+		return (r);
+}
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+void	*ft_memmove(void *dest, const void *src, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	if (!dest && !src)
+		return (0);
+	if (dest > src)
+	{
+		while (n)
+		{
+			n--;
+			*((unsigned char *)(dest + n)) = *((unsigned char *)(src + n));
+		}
+	}
+	else
+	{
+		while (i < n)
+		{
+			*((unsigned char *)(dest + i)) = *((unsigned char *)(src + i));
+			i++;
+		}
+	}	
+	return (dest);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	len1;
+	size_t	len2;
+	char	*join;
+
+	if (!s1 || !s2)
+		return (NULL);
+	len1 = ft_strlen(s1);
+	len2 = ft_strlen(s2);
+	join = malloc(len1 + len2 + 1 * sizeof(char));
+	if (!join)
+		return (NULL);
+	ft_memmove(join, s1, len1);
+	ft_memmove(join + len1, s2, len2 + 1);
+	return (join);
+}
+
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 32
+#  define BUFFER_SIZE 4096
 # endif
 
 char	*get_next_line(int fd);
